fb1d8eb5871d7c25c5b6bf04f75d1dc347f83d87
Niharika Sondagar <ngirishbhaisondagar026@gmail.com>
2026-09-14 12:31:10 +0530

    Test backed1

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const cors = require("cors");
 
 const connectDB = require("./backend/config/db");
 
+const authRoutes = require("./backend/routes/authRoutes");
+const patientRoutes = require("./backend/routes/patientRoutes");
+const hospitalRoutes = require("./backend/routes/hospitalRoutes");
+const ambulanceRoutes = require("./backend/routes/ambulanceRoutes");
+const emergencyRoutes = require("./backend/routes/emergencyRoutes");
+
 dotenv.config();
 
 connectDB();
@@ -12,6 +18,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/patients", patientRoutes);
+app.use("/api/hospitals", hospitalRoutes);
+app.use("/api/ambulances", ambulanceRoutes);
+app.use("/api/emergencies", emergencyRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hospital Emergency Management API is running");
